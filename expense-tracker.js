ad882f59eb809c922273ebd8e1eea66490b50c3e
CrazedBySerenity <villeisakd@gmail.com>
2023-03-21 10:20:13 +0100

    Finished adding buttons being toggled visible depending on if they are to be used. Removed set nam button as it was redundant. Added function to output summary into the console

--- expense-tracker.js
+++ expense-tracker.js
@@ -11,6 +11,9 @@ const showSummaryText = "Here is your summary:";
 
 const textInput = document.getElementById("form-text");
 const numbersInput = document.getElementById("form-numbers");
+const buttonInput = document.getElementById("form-btn");
+
+const displayStyle = "inline-block";
 
 //0 = input name
 //1 = add income
@@ -39,8 +42,15 @@ const account = {
     },
 
     showSummary(){
-        this.expenses.foreach(element => console.log(`type: ${element.description} amount: ${element.amount}€`));
-        this.income.foreach(element => console.log(`type: ${element.description} amount: ${element.amount}€`));
+
+        //Replace console output with the creation of new elements that display the description and amount
+        for(i = 0; i < this.expenses.length; i++){
+            console.log(`type: ${this.expenses[i].description} amount: ${this.expenses[i].amount}€`);
+        }
+        
+        for(i = 0; i < this.income.length; i++){
+            console.log(`type: ${this.income[i].description} amount: ${this.income[i].amount}€`);
+        }
     },
 }
 
@@ -50,6 +60,10 @@ menu(0);
 function menu(newAction){
     currentAction = newAction;
     let explanationText;
+
+    textInput.style.display = "none";
+    numbersInput.style.display = "none";
+    buttonInput.style.display = "none";
 
     if(account.holderName != ""){
         document.getElementById("welcome-text").textContent = " " + account.holderName;
@@ -63,16 +77,24 @@ function menu(newAction){
         case 0:
             explanationText = setNameText;
             document.getElementById("welcome-text").textContent = " dear customer";
+            textInput.style.display = displayStyle;
+            buttonInput.style.display = displayStyle;
             break;
         case 1:
             explanationText = addIncomeText;
+            textInput.style.display = displayStyle;
+            numbersInput.style.display = displayStyle;
+            buttonInput.style.display = displayStyle;
             break;
         case 2:
             explanationText = addExpenseText;
+            textInput.style.display = displayStyle;
+            numbersInput.style.display = displayStyle;
+            buttonInput.style.display = displayStyle;
             break;
         case 3:
             explanationText = showSummaryText;
-            showSummary();
+            account.showSummary();
             break;
     }
     document.getElementById("explanation-text").textContent = explanationText;
@@ -90,7 +112,6 @@ function submitInput() {
         }
     
         else    {  
-            //main logic here
 
             switch(currentAction){
                 case 0:
@@ -100,12 +121,15 @@ function submitInput() {
                     break;
                 case 1:
                     account.addIncome(text, numbers);
+                    account.totalIncome += parseInt(numbers);
                     break;
                 case 2:
                     account.addExpenses(text, numbers);
+                    account.totalExpenses += parseInt(numbers);
                     break;
                 case 3:
                     account.expenses.forEach(element => console.log(element.description + " " + element.amount));
+                    
                     break;
             }
 
