281f65ed45db313ef06e6469f308267da9c2386a
CrazedBySerenity <villeisakd@gmail.com>
2023-03-27 12:28:40 +0200

    Worked on the appearance, added checks to see if the arrays are empty before trying to display the information contained within them. Added better and clearer error messages. Added labels for the input

--- scripts/expense-tracker.js
+++ scripts/expense-tracker.js
@@ -9,12 +9,15 @@ const addExpenseText = "Add an expense";
 const showSummaryText = "Here is your summary:";
 
 const textInput = document.getElementById("form-text");
+const textLabel = document.getElementById("form-text-label");
 const numbersInput = document.getElementById("form-numbers");
+const numbersLabel = document.getElementById("form-numbers-label")
 const buttonInput = document.getElementById("form-btn");
 const summary = document.getElementById("summary");
 const explainer = document.getElementById("explanation-text");
 
 const displayStyle = "inline-block";
+const errorColor = "darkred";
 
 //0 = input name
 //1 = add income
@@ -33,7 +36,7 @@ const account = {
         {description: "food", amount: 243},
         {description: "dog", amount: 339},
     ],
-    totalExpenses: 732,
+    totalExpenses: 682,
     income: [
         {description: "job", amount: 1000},
         {description: "gift", amount: 52},
@@ -55,7 +58,17 @@ const account = {
     },
 
     showSummary(){
-        //Add a check for if the arrays are empty
+        if(this.expenses.length < 1 && this.income.length < 1){
+            console.log("No expenses or income added");
+            explainer.textContent = "You have not added any income or expenses yet";
+            explainer.style.color = errorColor;
+            return
+        }
+
+        if(this.expenses.length < 1){
+            console.log("No expenses added");
+            return
+        }
 
         document.getElementById("total-balance").textContent = this.totalIncome - this.totalExpenses;
         
@@ -84,6 +97,11 @@ const account = {
 
         //Quick solution to avoid creating duplicate elements for now
         this.expenses.length = 0;
+
+        if(this.income.length < 1){
+            console.log("No income added");
+            return
+        }
         
         for(i = 0; i < this.income.length; i++){
             console.log(`type: ${this.income[i].description} amount: ${this.income[i].amount}€`);
@@ -118,8 +136,11 @@ function menu(newAction){
 
     textInput.style.display = "none";
     numbersInput.style.display = "none";
+    textLabel.style.display = "none";
+    numbersLabel.style.display = "none";
     buttonInput.style.display = "none";
     summary.style.display = "none";
+    explainer.style.color = "white";
 
     if(account.holderName != ""){
         document.getElementById("welcome-text").textContent = " " + account.holderName;
@@ -141,12 +162,16 @@ function menu(newAction){
             textInput.style.display = displayStyle;
             numbersInput.style.display = displayStyle;
             buttonInput.style.display = displayStyle;
+            textLabel.style.display = displayStyle;
+            numbersLabel.style.display = displayStyle;
             break;
         case 2:
             explanationText = addExpenseText;
             textInput.style.display = displayStyle;
             numbersInput.style.display = displayStyle;
             buttonInput.style.display = displayStyle;
+            textLabel.style.display = displayStyle;
+            numbersLabel.style.display = displayStyle;
             break;
         case 3:
             explanationText = showSummaryText;
@@ -171,15 +196,21 @@ function submitInput() {
         //The numbers field being missing (while required)
         //The numberS field not being a number (while required)
 
+        if(numbers < 1 && currentAction != 0){
+            explainer.textContent = currentExplainer + ", your transaction amount cannot be less than zero";
+            explainer.style.color = errorColor;
+            console.log("Invalid input");
+            return;
+        }
+
         if(!text || !numbers && currentAction != 0) {
             explainer.textContent = currentExplainer + ", you cannot submit an empty input";
+            explainer.style.color = errorColor;
+            console.log("Invalid input");
             return;
         }
 
         else if(text.length < minTextInput || !text || !numbers && currentAction != 0) {
-            
-            //Add a real error message
-            //Make the error message contextual to the real error
             //Outputting debug into the console and preventing any further code in the function from being run
             console.log("Invalid input");
             return;
@@ -210,7 +241,6 @@ function submitInput() {
                     account.expenses.forEach(element => console.log(element.description + " " + element.amount));
                     break;
             }
-
         }
     }
 
