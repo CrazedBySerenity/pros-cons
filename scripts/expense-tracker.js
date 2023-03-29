e871a034de2bc66b50fb6c4b951f74016e38942e
CrazedBySerenity <villeisakd@gmail.com>
2023-03-29 15:29:24 +0200

    Reworked the expense-tracker into a site to make a pros and cons list. Most of the obsolete code used has been commented out, not deleted

--- scripts/expense-tracker.js
+++ scripts/expense-tracker.js
@@ -5,15 +5,15 @@ let currentExplainer = "";
 let summaryIsEmpty = true;
 let confirmationTextActive = false;
 
-const setNameText = "Please set a name for your account";
-const addIncomeText = "Add an income";
-const addExpenseText = "Add an expense";
-const showSummaryText = "Here is your summary:";
+const setNameText = "What's the subject?";
+const addIncomeText = "Add a pro";
+const addExpenseText = "Add a con";
+const showSummaryText = "Decision summary:";
 
 const textInput = document.getElementById("form-text");
-const textLabel = document.getElementById("form-text-label");
-const numbersInput = document.getElementById("form-numbers");
-const numbersLabel = document.getElementById("form-numbers-label")
+// const textLabel = document.getElementById("form-text-label");
+// const numbersInput = document.getElementById("form-numbers");
+// const numbersLabel = document.getElementById("form-numbers-label")
 const buttonInput = document.getElementById("form-btn");
 const summary = document.getElementById("summary");
 const explainer = document.getElementById("explanation-text");
@@ -27,7 +27,7 @@ const errorColor = "darkred";
 //0 = input name
 //1 = add income
 //2 = add expenses
-//3 = show all expenses
+//3 = show all expenses and income
 
 const summaryElements = {
     income: [],
@@ -37,25 +37,17 @@ const summaryElements = {
 const account = {
     holderName: "",
     //Placeholder values to test the summary display without having to enter new incomes/expenses every time, the total expenses+incomes are also updated with the values already added
-    expenses: [
-        {description: "gas", amount: 100},
-        {description: "food", amount: 243},
-        {description: "insurance", amount: 339},
-    ],
-    totalExpenses: 682,
-    income: [
-        {description: "salary", amount: 1000},
-        {description: "gift", amount: 52},
-    ],
-    totalIncome: 1052,
+    expenses: [],
 
-    addExpenses(description, amount) {
-        let newExpense = {description: description, amount: amount};
+    income: [],
+
+    addExpenses(description) {
+        let newExpense = {description: description};
         this.expenses.push(newExpense);
     },
 
-    addIncome(description, amount) {
-        let newIncome = {description: description, amount: amount};
+    addIncome(description) {
+        let newIncome = {description: description};
         this.income.push(newIncome);
     },
 
@@ -65,18 +57,17 @@ const account = {
 
     showSummary(){
 
-        document.getElementById("total-balance").textContent = this.totalIncome - this.totalExpenses;
+        // document.getElementById("total-balance").textContent = this.totalIncome - this.totalExpenses;
 
         //A check to make sure the arrays are not empty
         if(this.expenses.length < 1 && this.income.length < 1 && summaryIsEmpty == true){
-            console.log("No expenses or income added");
             explainer.style.color = errorColor;
-            return("You have not added any income or expenses yet");
+            return("You haven't added any pros or cons yet");
         }
 
         //Checking to see if the expenses array is empty, if not then create the elements to the display it's information
         if(this.expenses.length < 1){
-            console.log("No expenses added");
+            console.log("No cons added");
         }
         
         //To-do: Add a check for if the correct element already exists before creating a new one
@@ -84,8 +75,6 @@ const account = {
         else{
 
             for(i = 0; i < this.expenses.length; i++){
-                console.log(`type: ${this.expenses[i].description} amount: ${this.expenses[i].amount}€`);
-                
                 let container = document.getElementById("expenses-container");
     
                 let newCard = document.createElement("div");
@@ -97,10 +86,10 @@ const account = {
                 descText.textContent = this.expenses[i].description;
                 newCard.appendChild(descText);
     
-                let amountText = document.createElement("p");
-                amountText.setAttribute("class", "amount__text");
-                amountText.textContent = this.expenses[i].amount;
-                newCard.appendChild(amountText);
+                // let amountText = document.createElement("p");
+                // amountText.setAttribute("class", "amount__text");
+                // amountText.textContent = this.expenses[i].amount;
+                // newCard.appendChild(amountText);
     
                 summaryElements.expenses.push(newCard);
             }
@@ -112,14 +101,12 @@ const account = {
 
         //Same check as for expenses
         if(this.income.length < 1){
-            console.log("No income added");
+            console.log("No pros added");
         }
 
         else {
 
             for(i = 0; i < this.income.length; i++){
-                console.log(`type: ${this.income[i].description} amount: ${this.income[i].amount}€`);
-    
                 let container = document.getElementById("income-container");
     
                 let newCard = document.createElement("div");
@@ -131,10 +118,10 @@ const account = {
                 descText.textContent = this.income[i].description;
                 newCard.appendChild(descText);
     
-                let amountText = document.createElement("p");
-                amountText.setAttribute("class", "amount__text");
-                amountText.textContent = this.income[i].amount;
-                newCard.appendChild(amountText);
+                // let amountText = document.createElement("p");
+                // amountText.setAttribute("class", "amount__text");
+                // amountText.textContent = this.income[i].amount;
+                // newCard.appendChild(amountText);
     
                 summaryElements.income.push(newCard);
             }
@@ -153,9 +140,9 @@ function menu(newAction){
 
     //Hide all elements by default, so that they can later be displayed as needed
     textInput.style.display = "none";
-    numbersInput.style.display = "none";
-    textLabel.style.display = "none";
-    numbersLabel.style.display = "none";
+    // numbersInput.style.display = "none";
+    // textLabel.style.display = "none";
+    // numbersLabel.style.display = "none";
     buttonInput.style.display = "none";
     summary.style.display = "none";
     explainer.style.color = "black";
@@ -172,23 +159,23 @@ function menu(newAction){
     }
 
     else {
-        document.getElementById("welcome-text").textContent = " dear user";
+        document.getElementById("welcome-text").textContent = "Pros and Cons";
     }
 
     switch(currentAction){
         case 0:
             explanationText = setNameText;
-            document.getElementById("welcome-text").textContent = " dear user";
+            document.getElementById("welcome-text").textContent = "Pros and cons";
             textInput.style.display = displayStyle;
             buttonInput.style.display = displayStyle;
             break;
         case 1:
             explanationText = addIncomeText;
             textInput.style.display = displayStyle;
-            numbersInput.style.display = displayStyle;
+            // numbersInput.style.display = displayStyle;
             buttonInput.style.display = displayStyle;
-            textLabel.style.display = displayStyle;
-            numbersLabel.style.display = displayStyle;
+            // textLabel.style.display = displayStyle;
+            // numbersLabel.style.display = displayStyle;
             //Disable button when the menu is active
             incomeButton.style.pointerEvents = "none";
             incomeButton.disabled = true;
@@ -196,10 +183,10 @@ function menu(newAction){
         case 2:
             explanationText = addExpenseText;
             textInput.style.display = displayStyle;
-            numbersInput.style.display = displayStyle;
+            // numbersInput.style.display = displayStyle;
             buttonInput.style.display = displayStyle;
-            textLabel.style.display = displayStyle;
-            numbersLabel.style.display = displayStyle;
+            // textLabel.style.display = displayStyle;
+            // numbersLabel.style.display = displayStyle;
             //Disable button when the menu is active
             expensesButton.style.pointerEvents = "none";
             expensesButton.disabled = true;
@@ -218,11 +205,11 @@ function menu(newAction){
 }
 
 function submitInput() {
-    numbers = numbersInput.value;
+    // numbers = numbersInput.value;
     text = textInput.value;
  
     //Validation check to make sure the input exists
-    if(text != null && numbers != null){
+    if(text != null/* && numbers != null*/){
 
         //Validation checks for:
         //The text input being shorter than the minimum allowed amount of characters
@@ -232,21 +219,21 @@ function submitInput() {
         //
         //If the input is invalid an error message is output into the console, the user is notified and the function does not execute any further code
 
-        if(numbers < 1 && currentAction != 0){
-            explainer.textContent = currentExplainer + ", your transaction amount cannot be less than zero";
+        // if(numbers < 1 && currentAction != 0){
+        //     explainer.textContent = currentExplainer + ", your transaction amount cannot be less than zero";
+        //     explainer.style.color = errorColor;
+        //     console.log("Invalid input");
+        //     return;
+        // }
+
+        if(!text/* || !numbers && currentAction != 0*/) {
+            explainer.textContent = currentExplainer + "\r\nYou cannot submit an empty input";
             explainer.style.color = errorColor;
             console.log("Invalid input");
             return;
         }
 
-        if(!text || !numbers && currentAction != 0) {
-            explainer.textContent = currentExplainer + ", you cannot submit an empty input";
-            explainer.style.color = errorColor;
-            console.log("Invalid input");
-            return;
-        }
-
-        else if(text.length < minTextInput || !text || !numbers && currentAction != 0) {
+        else if(text.length < minTextInput || !text/* || !numbers && currentAction != 0*/) {
             
             console.log("Invalid input");
             return;
@@ -261,26 +248,26 @@ function submitInput() {
                     document.getElementById("welcome-text").textContent = " " + account.holderName;
                     document.getElementById("button-container").style.display = "grid";
                     textInput.style.display = "none";
-                    numbersInput.style.display = "none";
+                    // numbersInput.style.display = "none";
                     buttonInput.style.display = "none";
                     summary.style.display = "none";
                     explainer.textContent = "Pick an option using the buttons";
                     explainer.style.color = "black";
                     break;
                 case 1:
-                    account.addIncome(text, numbers);
-                    account.totalIncome += parseInt(numbers);
+                    account.addIncome(text);
+                    // account.totalIncome += parseInt(numbers);
                     showTransactionConfirmation();
                     setTimeout(showTransactionConfirmation, 1000);
                     break;
                 case 2:
-                    account.addExpenses(text, numbers);
-                    account.totalExpenses += parseInt(numbers);
+                    account.addExpenses(text);
+                    // account.totalExpenses += parseInt(numbers);
                     showTransactionConfirmation();
                     setTimeout(showTransactionConfirmation, 1000);
                     break;
                 case 3:
-                    account.expenses.forEach(element => console.log(element.description + " " + element.amount));
+                    // account.expenses.forEach(element => console.log(element.description + " " + element.amount));
                     break;
             }
         }
